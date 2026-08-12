e090a6837a0a8efdca3cbce544bc5f7ff4151875
Nima <alikhahnima@gmail.com>
2026-08-12 16:44:15 +0330

    feature:add category model

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,33 @@
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError, transaction
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Category
+
+
+class CategoryModelTests(TestCase):
+    def test_string_representation_is_name(self):
+        category = Category(name="Electronics")
+
+        self.assertEqual(str(category), "Electronics")
+
+    def test_categories_are_ordered_by_name(self):
+        Category.objects.create(name="Tools")
+        Category.objects.create(name="Electronics")
+
+        names = list(Category.objects.values_list("name", flat=True))
+
+        self.assertEqual(names, ["Electronics", "Tools"])
+
+    def test_name_cannot_be_blank(self):
+        category = Category(name="")
+
+        with self.assertRaises(ValidationError):
+            category.full_clean()
+
+    def test_name_must_be_unique(self):
+        Category.objects.create(name="Electronics")
+
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                Category.objects.create(name="Electronics")
